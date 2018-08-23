df58ce524bcee3466fe006226fcec87e4c4e036d
eohyemin <eohyemin@gmail.com>
2018-08-23 14:19:38 +0900

    haemin

---
+++ SelluvProject/src/main/java/com/company/selluv/controller/LikeDeleteController.java
@@ -0,0 +1,26 @@
+package com.company.selluv.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.selluv.service.LikeService;
+
+@RestController
+public class LikeDeleteController {
+
+	@Autowired
+	LikeService likeService;
+	
+	@RequestMapping(value="likeDelete.do", method=RequestMethod.POST)
+	public String likeDelete(@RequestParam("memberId") String memberId, @RequestParam("contentsCode") String contentCode) {
+		boolean res = likeService.likeDelete(memberId, contentCode);
+		if(res) {
+			return "{\"flag\":\"n\"}";
+		}
+		return null;
+	}
+	
+}
